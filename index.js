8f98e09ff408e06dbf328e45f0e55ac7c9367b86
Nowrin Nazim <diya.nowrin08@gmail.com>
2021-11-19 02:58:56 +0600

    deploy2 commit

--- index.js
+++ index.js
@@ -94,6 +94,14 @@ async function run() {
             res.send(orders);
         });
 
+        // order delete
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.send(result);
+        });
+
 
         //Register
         app.post('/users', async (req, res) => {
